293e7b6a26f04251286211f0ec7c6d3f10eb20bb
yxl <yxl@yxldeMacBook-Pro.local>
2019-03-01 15:49:09 +0800

    初始化文件

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  extends: 'airbnb-base',
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    // allow console during development
+    'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': [
+      'error',
+        {
+          anonymous: 'always',
+          named: 'always',
+          asyncArrow: 'always',
+        },
+      ]
+  },
+  'globals': {
+    document: true,
+    location: true,
+    window: true,
+  },
+}
---
+++ src/static/js/main.js
@@ -0,0 +1,6 @@
+$(function(){
+
+  let demo = 123;
+  console.log( demo );
+
+})
